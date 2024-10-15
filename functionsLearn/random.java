d0b98f51c1245524d133c2624dfcd543d88aa5a8
Mithunns01 <123565113+Mithunns01@users.noreply.github.com>
2024-10-15 15:30:16 +0530

    Add files via upload

---
+++ functionsLearn/random.java
@@ -0,0 +1,8 @@
+package functionsLearn;
+
+public class random {
+    public static void main(String[] args) {
+        int diceRoll = (int) (Math.random()*6);
+        System.out.println("You rolled a " + diceRoll);
+    }
+}
